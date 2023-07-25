c6db6d268d53e82cf99a5f7741a2f3a56798d0b4
mustapha <stephanoallali75@gmail.com>
2023-07-25 15:03:27 +0200

    CRUD products

--- Models/Users.php
+++ Models/Users.php
@@ -17,7 +17,52 @@ class Users extends Db{
 
 
     //  2 - Méthode pour trouver un user par son id
+
+    public static function findById($id) {
+        $request = 'SELECT * FROM users WHERE idUser = :id';
+        $response = self::getDb()->prepare($request);
+        $response->bindValue(':id', $id, PDO::PARAM_INT);
+        $response->execute();
+
+        return $response->fetch(PDO::FETCH_ASSOC);
+    }
+
+    
     //  3 - Méthode pour trouver un user par son login
 
+    public static function findByLogin($login){
+        $request = 'SELECT * FROM users WHERE login = :login';
+        $response = self::getDb()->prepare($request);
+        $response->bindValue(':login', $login, PDO::PARAM_STR);
+        $response->execute();
 
+        return $response->fetch(PDO::FETCH_ASSOC);
+    }
+
+    ///////////// Méthode d'écriture
+    // 1 / Méthode pour créer un user
+    public static function create($data){
+    // Syntaxe sans les bindValue utilisation du "?"
+    // avec cette syntaxe $data doit etre un tableau qui contient toutes les valeurs à enregistrer en BDD
+        $request = "INSERT INTO users (login, password, firstName, lastName, adress, cp, city) VALUE (?, ?, ?, ?, ?, ?, ?)";
+        $response = self::getDb()->prepare($request);
+        return $response->execute($data);
+
+}
+
+// 2 / Méthode pour modifier un user
+
+public static function update(array $data){
+    $request = "UPDATE users SET login = ?, password = ?, firstName = ?, lastName = ?, adress = ?, cp = ?, city = ? WHERE idUser = ?";
+    $response = self::getDb()->prepare($request);
+    return $response->execute($data);
+}
+
+// 3 / Méthode de suppression
+public static function delete($id){
+    $request = "DELETE FROM users WHERE idUser = :id";
+    $response = self::getDb()->prepare($request);
+    $response->bindValue(':id', $id, PDO::PARAM_INT);
+    return $response->execute();
+}
 }
--- index.php
+++ index.php
@@ -1,25 +1,100 @@
 <?php
+
 use App\Db;
+use Models\Categories;
+use Models\Products;
 use Models\Users;
 
 
-require_once ("Autoloader.php");
-
-// test de l'autoloader
+require_once('autoloader.php');
+// Test de l'autoloader
 // $test = new Db;
 // $test::getDb();
+
+
+
+?>
+<h1>Index : fichier de test</h1>
+<p>C'est ici que nous allons tester tous nos CRUD</p>
+<!-- Pour utliser les méthodes des CRUD il faut faire un require des class dont nous aurons besoin -->
+<!-- Comme nous ne voulons pas faire des require toutes les deux minutes nous allons utiliser un autoloader -->
+
+<h2>Utilisation de la méthode findAll sur users</h2>
+<?php
+    $users = Users::findAll();
+    var_dump($users);
+?>
+<h2>Utilisation de la méthode findById sur users</h2>
+<?php
+    $user = Users::findById(2);
+    var_dump($user);
+?>
+<h2>Utilisation de la méthode findByLogin sur users</h2>
+<?php
+    $login = "user1@gmail.com";
+    $user = Users::findByLogin($login);
+    var_dump($user);
+?>
+
+<h2>Utilisation de la methode create() sur users</h2>
+<?php
+    $pass = password_hash('12345678', PASSWORD_DEFAULT);
+    $data = ['sylvain.evogue@gmail.com', $pass, 'sylvain', 'campos', '21 avenue des yvris', '93160', 'noisy-le-grand',3];
+
+    // Users::create($data);
+    // Users::update($data);
 ?>
 
 
-
-
-<h1>index : fichier de test</h1>
-<p>c'est ici que nous allons tester tous nos CRUD</p>
-<!-- Pour utiliser les méthodes des CRUD il faut faire un require des class dont nous aurons besoin  -->
-<!-- Comme ne nous voulons pas faire des require toutes  les deux minutes nous allons utiliser un autoloader-->
-
-<h2>Utilisation de la méthode findALL sur users</h2>
+<h2>Test de la méthode findAll sur catégories</h2>
 <?php
-$users = Users::findALL();
-var_dump($users);
+    $categories = Categories::findAll();
+    var_dump($categories);
+?>
+<h2>Test de la méthode findById sur catégories</h2>
+<?php
+    $categories = Categories::findById(2);
+    var_dump($categories);
+?>
+<h2>Test de la méthode create sur catégories</h2>
+<?php
+    // $categories = Categories::create("non classé");   
+?>
+<h2>Test de la méthode update sur catégories</h2>
+<?php
+    // $categories = Categories::update("non classée",1);   
+?>
+<h2>Test de la méthode delete sur catégories</h2>
+<?php
+    // $categories = Categories::delete(1);   
+?>
+
+<h2>Test de la méthode findAll sur Product</h2>
+<?php
+$products = Products::findAll(null, 1);
+var_dump($products);
+?>
+
+<h2>Test de la méthode findbyId sur Product</h2>
+<?php 
+$products = Products::findbyId(1);
+var_dump($products);
+?>
+
+<h2>Test de la méthode findbyUser sur Product</h2>
+<?php 
+$products = Products::findbyUser(1);
+var_dump($products);
+?>
+
+<h2>Utilisation de la methode create() sur users</h2>
+<?php
+  
+    $data = [2, 3, 'iphone', 'le tout dernier', '1200', 'iphone.jpg'];
+    Products::create(($data));
+
+    // Users::create($data);
+    // Users::update($data);
+?>
+
 
